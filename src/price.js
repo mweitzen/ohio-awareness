457e975b098c41ec56e2cb614f0df5008096d55b
Michael Weitzenhoffer <michaelweitzenhoffer@michaels-mbp.mynetworksettings.com>
2025-02-26 19:14:20 -0500

    adjusting script execution

--- src/price.js
+++ src/price.js
@@ -1,43 +1,45 @@
-// Get the user type from the query parameter
-const urlParams = new URLSearchParams(window.location.search);
-const userType = urlParams.get('role');
+document.addEventListener('DOMContentLoaded', () => {
+  // Get the user type from the query parameter
+  const urlParams = new URLSearchParams(window.location.search);
+  const userType = urlParams.get('role');
 
-const earlyBirdCutoff = new Date('2025-03-03');
-const currentDate = new Date();
-const isEarlyBird = currentDate < earlyBirdCutoff;
+  const earlyBirdCutoff = new Date('2025-03-03');
+  const currentDate = new Date();
+  const isEarlyBird = currentDate < earlyBirdCutoff;
 
-// Calculate the price based on the user type
-let price = 0;
-switch (userType) {
-  case 'doctor':
-    price = 800;
-    break;
-  case 'nurse':
-    price = 400;
-    break;
-  default:
-    price = 0;
-}
+  // Calculate the price based on the user type
+  let price = 0;
+  switch (userType) {
+    case 'doctor':
+      price = 800;
+      break;
+    case 'nurse':
+      price = 400;
+      break;
+    default:
+      price = 0;
+  }
 
-if (isEarlyBird) {
-  price = price - 50;
-}
+  if (isEarlyBird) {
+    price = price - 50;
+  }
 
-// Show early bird discount
-if (isEarlyBird) {
-  const discountContainer = document.getElementById('early-bird');
-  discountContainer.classList.remove('hidden');
-}
+  // Show early bird discount
+  if (isEarlyBird) {
+    const discountContainer = document.getElementById('early-bird');
+    discountContainer.classList.remove('hidden');
+  }
 
-// Display the price to the user
-const priceContainer = document.getElementById('price');
-const regularPriceHtml = `<span class="line-through text-muted-foreground">$${
-  price + 50
-}</span>`;
-const innerHTML = `Price: ${isEarlyBird ? regularPriceHtml : ''} $${price}`;
-priceContainer.innerHTML = innerHTML;
+  // Display the price to the user
+  const priceContainer = document.getElementById('price');
+  const regularPriceHtml = `<span class="line-through text-muted-foreground">$${
+    price + 50
+  }</span>`;
+  const innerHTML = `Price: ${isEarlyBird ? regularPriceHtml : ''} $${price}`;
+  priceContainer.innerHTML = innerHTML;
 
-// Update the pay button with the price
-const payButton = document.getElementById('pay');
-payButton.innerHTML = 'Pay: $' + price;
-payButton.value = price;
+  // Update the pay button with the price
+  const payButton = document.getElementById('pay');
+  payButton.innerHTML = 'Pay: $' + price;
+  payButton.value = price;
+});
