5a6353fc91799ea397dd13738ed2994f592caec4
Michael Weitzenhoffer <michaelweitzenhoffer@michaels-mbp.mynetworksettings.com>
2025-03-03 19:58:40 -0500

    Add internal email'

--- netlify/functions/confirmation/confirmation.mjs
+++ netlify/functions/confirmation/confirmation.mjs
@@ -15,8 +15,20 @@ export const handler = async (request) => {
     html: '<p><strong>Thank You!</strong></p> <p>You have successfully registered for the MBSR course</p>',
   };
 
+  // "rweiner1@gmail.com",
+  const emails = ['ewein00@gmail.com', 'mweitzenhoffer@gmail.com'];
+
+  const internalMsg = {
+    to: emails,
+    from: 'rweiner1@gmail.com',
+    subject: 'Someone registered for the 5/5/2025 session!',
+    text: `New Registration! Someone just registered for the 5/5/2025 session with email: ${email}`,
+    html: `<p><strong>New Registration!</strong></p> <p>Someone just registered for the 5/5/2025 session with email: ${email}</p>`,
+  };
+
   try {
     await sgMail.send(msg);
+    await sgMail.send(internalMsg);
   } catch (error) {
     console.error(error);
 
