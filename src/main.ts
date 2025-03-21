7ffdfb8d79ec5291bf81b6a18fed5ba3bcf62ad0
Michael Weitzenhoffer <michaelweitzenhoffer@michaels-mbp.mynetworksettings.com>
2025-03-21 18:46:30 -0400

    Making ridiculous hot fix changes

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const now = new Date();
-  const earlyBirdDate = new Date('03-10-2025');
-
-  const element = document.getElementById('early-bird-display');
-
-  if (element === null) {
-    throw new Error('Element not found');
-  }
-
-  if (earlyBirdDate < now) {
-    element.style.display = 'none';
-  }
+  // const now = new Date();
+  // const earlyBirdDate = new Date('03-10-2025');
+  // const element = document.getElementById('early-bird-display');
+  // if (element === null) {
+  //   throw new Error('Element not found');
+  // }
+  // if (earlyBirdDate < now) {
+  //   element.style.display = 'none';
+  // }
 });
