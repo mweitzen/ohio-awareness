5407de19f8af490373e015f9bcd96f5e0a4ee269
Michael Weitzenhoffer <michaelweitzenhoffer@michaels-mbp.mynetworksettings.com>
2025-03-25 18:20:36 -0400

    Adjust date in the email

--- netlify/functions/confirmation/confirmation.mjs
+++ netlify/functions/confirmation/confirmation.mjs
@@ -11,7 +11,7 @@ export const handler = async (request) => {
     to: email,
     from: 'rweiner1@gmail.com',
     subject: 'MBSR Registration Confirmed!',
-    text: 'Thank you! You have successfully registered for the MBSR course',
+    text: 'Thank you! You have successfully registered for the MBSR course on 7/12/2025.',
     html: '<p><strong>Thank You!</strong></p> <p>You have successfully registered for Beyond MBSR.</p><p class="mb-6 font-light sm:text-lg text-base"><span>When:</span> Saturday, April 5, 2025 <br /> 9:00am-4:00pm - Bagels & Coffee at 8:30am <br /> <span >Where:</span> University Hospital Seidman Cancer Center Main Campus <br /> 11100 Euclid Ave, Cleveland, OH 44106 | Meditation Room <br /> Lunch Provided </p>',
   };
 
@@ -24,8 +24,8 @@ export const handler = async (request) => {
   const internalMsg = {
     to: emails,
     from: 'rweiner1@gmail.com',
-    subject: 'Someone registered for the 5/5/2025 session!',
-    text: `New Registration! Someone just registered for the 5/5/2025 session with email: ${email}`,
+    subject: 'Someone registered for the 7/12/2025 session!',
+    text: `New Registration! Someone just registered for the 7/12/2025 session with email: ${email}`,
     html: `<p><strong>New Registration!</strong></p> <p>Someone just registered for the 5/5/2025 session with email: ${email}</p>`,
   };
 
